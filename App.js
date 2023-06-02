268e30170bf75b6107b67dbf2860422e44c399ab
sanich123 <aovoronin.piano@gmail.com>
2023-06-02 15:15:27 +0300

    feat: work on launch the app

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import Mapbox from "@rnmapbox/maps";
+import "expo-dev-client";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import Mapbox, { PointAnnotation, Callout, MapView, Camera } from "@rnmapbox/maps";
 
-Mapbox.setWellKnownTileServer('Mapbox');
+Mapbox.setWellKnownTileServer("Mapbox");
 Mapbox.setAccessToken("pk.eyJ1Ijoic2FuaWNoMTIzIiwiYSI6ImNsaWFkNmptaDAyaTczcm11NHF0cmp3d2sifQ.ZKH9THateIfnZ7zC23f3-g");
 
-const App = () => {
+export default function App() {
+  const [calloutVisible, setCalloutVisible] = useState(false);
+  const [coordinates] = useState([-5, 55]);
+  const onMarkerPress = () => {
+    setCalloutVisible(true);
+  };
+
+  const loadAnnotationUK = () => {
+    return (
+      <PointAnnotation key="annotationUK" id="annotationUK" coordinate={[0.1, 51.5]} onSelected={onMarkerPress}>
+        <View
+          style={{
+            height: 20,
+            width: 20,
+            backgroundColor: "green",
+            borderColor: "black",
+            borderWidth: 2,
+            borderRadius: 50,
+          }}
+        ></View>
+
+        <Callout title="Welcome to London!" contentStyle={{ borderRadius: 5 }}></Callout>
+      </PointAnnotation>
+    );
+  };
+
   return (
     <View style={styles.page}>
       <View style={styles.container}>
-        <Mapbox.MapView style={styles.map} />
+        <MapView style={styles.map}>
+          <Camera zoomLevel={4} centerCoordinate={coordinates} />
+          <PointAnnotation id="uk" coordinate={coordinates} />
+          <View>{loadAnnotationUK()}</View>
+        </MapView>
       </View>
     </View>
   );
 };
-
-export default App;
 
 const styles = StyleSheet.create({
   page: {
@@ -22,10 +51,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+
   container: {
-    height: 300,
-    width: 300,
+    height: "100%",
+    width: "100%",
   },
+
   map: {
     flex: 1,
   },
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# react-native-mapbox
+To watch our wonderful android app, you should go by these steps:
+
+1. If you dont't have eas-cli = {npm, yarn} install -g eas-cli
+
+2. Install the dependencies = {npm, yarn} install
+//npx expo install expo-dev-client expo-location must be
+
+3. Build an apk file = {npm run, yarn} buildApk
+//eas build --profile development --platform android
+
+3.1 When it will be necessary, type the following in the command line:
+
+'user' : sanich123
+'password' : 7fwd7rlm
+
+4. With QR-code or the direct link download the app.
+
+5. To run development = {npm, yarn} run startDev
+//npx expo start --dev-client
+
+6. Scan the QR-code in ExpoGo
+
+7. Fucking enjoy your app into the mobile
+
+
